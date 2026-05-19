4cf7fd0188e4423779716f9f56d4c01d41530c55
inoue <inoue@PC54>
2026-05-19 16:28:23 +0900

    2

--- src/p01/Test01.java
+++ src/p01/Test01.java
@@ -5,6 +5,9 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("水野武蔵２");
 		System.out.println("井上");
+		int a = 50;
+		int b = a * 3;
+		System.out.println(b);
 	}
 
 }
